7a0a636736896d92d7520b34b6b717f869d3875c
Ayush Anshuli <ayushanshuli1@gmail.com>
2023-02-03 19:58:21 +0530

    .

---
+++ hey.py
@@ -0,0 +1,24 @@
+# a="963"
+# a=int(a)
+# a=float(a)
+# print(type(a))
+# print(a+3)
+# print(a+6)
+
+# n=input("enter the first number\n")
+# q=input("enter the second number\n")
+# l=int(n)+int(q)
+# print("the sum of the number is ",l)
+
+a=input("enter your age\n")
+# b=input("enter your age\n")
+
+if int(a)>=18:
+    print("You are adult")
+elif int(a)<18 and int(a)>10:
+    print("You are in the school")
+else:
+    print("work hard")
+    print("thank you")
+    
+print("good ni8")
---
+++ ypp.py
@@ -0,0 +1,24 @@
+# RANGE
+a=list(range(20))
+b=list(range(10,20,3))
+print(a,  b)
+
+# LOOPS
+# FOR LOOPS
+for i in range(1,11):
+    print(3*i)
+# HERE i =VARIABLE
+
+c=['ayush','sak','ypyy']
+for name in c:
+    print(name*3)
+
+# WHILE LOOPS
+n=6
+while n>=0:
+    print(n)
+    n-=1
+
+# STRING
+a="thisayush"
+print(a[5])
